2bba9cd374565a26e21e1f66b18c7efe58156146
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-05-05 17:27:45 -0700

    cleaning up analysis file

--- R/startup.R
+++ R/startup.R
@@ -2,4 +2,8 @@
   packageStartupMessage("Welcome to the bwg R package!")
 }
 
+.onLoad <- function(libname, pkgname){
+  try(rdrop2::drop_acc())
+}
+
 
--- Rscripts/analysis.R
+++ Rscripts/analysis.R
@@ -6,25 +6,11 @@ read_site_sheet("Macae", "leaf.waterdepths")
 
 leaf_depth_list <- get_all_sites(sheetname = "leaf.waterdepths")
 
-
-## checking names
-check_names(leaf_depth_list[[1]])
-
-names(leaf_depth_list[[2]])[1] <- "a"
-names(leaf_depth_list[[2]])[3] <- "b"
-
-lapply(leaf_depth_list, check_names)
-
-lapply(leaf_depth_list, function(x) all.equal(names(x), water_sheet_names))
-
-namelist <- lapply(leaf_depth_list, names)
-Reduce(intersect, namelist) ## Are there problems?
-
-identical(namelist[[1]], namelist[[2]])
-
 library(dplyr)
 
 all_leaf <- rbind_all(leaf_depth_list)
+## write a function that combines the data and also checks it
+## not in that order
 
 ## date to date
 ## control the day of the experiment for each bromeliad
@@ -39,7 +25,6 @@ library("tidyr")
 longwater <- . %>%
   gather("data_name", "depth", starts_with("depth")) %>%
   separate(data_name, into = c("depth_word", "leaf", "first_or_second","first")) %>%
-  filter(!is.na(depth)) %>%
   select(-depth_word, -first)
 
 
@@ -60,6 +45,7 @@ cr %>%
   mutate(day_of_exp = dense_rank(date)) %>%
   separate(trt.name, into = c("mu", "k"), sep = "k") %>%
   mutate(mu = extract_numeric(mu)) %>%
-  ggplot(aes(x = date, y = depth, colour = leaf, group = bromeliad.id)) + geom_line() +
+  ggplot(aes(x = date, y = depth,
+             colour = leaf, group = bromeliad.id)) + geom_line() +
   facet_grid(mu~k)
 
