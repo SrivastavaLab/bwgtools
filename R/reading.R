bba8e72b8f9121a9294e7dfd1ca58ff1c9f9001f
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-05-08 09:29:04 -0700

    added ondisk argument to allow for passing a local path, not only from Dropbox

--- R/reading.R
+++ R/reading.R
@@ -3,22 +3,28 @@
 #' this function reads all the sheets in an excel book on dropbox
 #' @param file the file name to fetch
 #' @param sheetname the sheet name you want. must match exactly.
+#' @param ondisk Are you reading this data from your machine or online?
 #' @param dest destination for download
 #' @export
-read_sheet <- function(file, sheetname = NULL, dest = tempdir(), ...) {
+read_sheet <- function(file, sheetname = NULL, ondisk = FALSE, dest = tempdir(), ...) {
   if (is.null(sheetname)) stop("c'mon give me a sheet name")
-  localfile <-  paste0(dest, "/", basename(file))
+
+  if(ondisk){
+    localfile <- file
+  } else {
+    localfile <-  paste0(dest, "/", basename(file))
+  }
 
   sheet <-  match.arg(sheetname, c("leaf.waterdepths"))
   f <- switch(sheet,
                leaf.waterdepths = leaf.waterdepths_read)
   if (file.exists(localfile)) {
-    print("you downloaded that file already! reading from disk")
-    f(localfile)
+    message("you downloaded that file already! reading from disk")
+    f(localfile, ...)
   } else {
     print("fetching from dropbox")
     rdrop2::drop_get(file, local_file = localfile, overwrite = TRUE)
-    f(localfile)
+    f(localfile, ...)
   }
 }
 
@@ -26,6 +32,7 @@ read_sheet <- function(file, sheetname = NULL, dest = tempdir(), ...) {
 #'
 #' this function reads one water depth sheet
 #'
+#' @param file_to_read Path to file to be read
 leaf.waterdepths_read <- function(file_to_read){
   readxl::read_excel(path = file_to_read,
                      sheet = "leaf.waterdepths",
@@ -43,13 +50,13 @@ leaf.waterdepths_read <- function(file_to_read){
 #' @param file the file name to fetch
 #' @param sheetname the sheet name you want. must match exactly.
 #' @export
-read_site_sheet <- function(sitename, sheetname = NULL){
+read_site_sheet <- function(sitename, sheetname = NULL, ...){
   folders <- "BWG Drought Experiment/raw data/"
   filename_start <- "Drought_data_"
   file_ext <- ".xlsx"
 
   file_wanted <- paste0(folders,filename_start,sitename,file_ext)
-  read_sheet(file_wanted, sheetname = sheetname)
+  read_sheet(file_wanted, sheetname = sheetname, ...)
 }
 
 #' Get all that data
