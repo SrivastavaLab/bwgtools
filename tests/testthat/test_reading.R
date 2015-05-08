a521b6ab4f943e892cfbe4ae634d0ec2d35cbb53
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-05-08 09:26:12 -0700

    adding tests for reading function

---
+++ tests/testthat/test_reading.R
@@ -0,0 +1,12 @@
+library(bwgtools)
+context("reading data")
+
+test_that("data is read correctly", {
+  data <- system.file("extdata","test_file.xlsx", package = "bwgtools")
+  expect_message(read_site_sheet("Macae", "leaf.waterdepths", ondisk = TRUE, file = data),
+                 "you downloaded that file already! reading from disk")
+  testdat <- read_site_sheet("Macae", "leaf.waterdepths", ondisk = TRUE, file = data)
+  expect_equal(nrow(testdat), 2)
+  stereotype <- dplyr::data_frame(a=1)
+  expect_equal(class(testdat), class(stereotype))
+})
