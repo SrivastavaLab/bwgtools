30dfc0b974eb0a281dcbc11054265a74251f41df
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-05-08 08:56:03 -0700

    chega de being clever. commenting out .onLoad and simply advising people how to do things in .onAttach

--- R/startup.R
+++ R/startup.R
@@ -1,9 +1,9 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to the bwg R package!")
+  packageStartupMessage("Welcome to the bwg R package! in order to obtain data from the BWG dropbox folder, you need to authorize R to access your dropbox. run the following commands: \n  library(rdrop2) \n   drop_acc() \n then enter your username and password. This should only need to be done once per directory.")
 }
 
-.onLoad <- function(libname, pkgname){
-  try(rdrop2::drop_acc())
-}
+# .onLoad <- function(libname, pkgname){
+#   tryCatch(rdrop2::drop_acc(), finally = print("not online"))
+# }
 
 
