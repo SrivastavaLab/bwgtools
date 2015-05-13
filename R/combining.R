c13b9bc58324d3c4e5a141fad65d4551d4f197c5
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-05-13 12:49:02 -0700

    combines bromelaid.physical data

--- R/combining.R
+++ R/combining.R
@@ -33,3 +33,35 @@ combine_site.weather <- function(){
   allsite <- rbind_all(site_weather)
   return(allsite)
 }
+
+#' Obtain the bromeliad.physical data
+#'
+#' @return data.frame of all bromeliad.physical tabs
+#' @export
+combine_bromeliad.physical <- function(){
+  site_weather <- get_all_sites(sheetname = "bromeliad.physical")
+
+  ## clean and message
+  message("CLEANING blank columns off of Columbia. is that still necessary?")
+  okcols <- which(names(site_weather[[3]]) %in% c("site", "trt.name", "mu.scalar", "k.scalar", "intended.mu",
+                                        "intended.k", "temporal.block", "bromeliad.id", "maxvol", "leaf.number",
+                                        "mean.diam", "catchment.area", "turbidity.initial", "oxygen.percent.initial",
+                                        "oxygen.conc.initial", "ph.initial", "chlorophyll.initial", "bacteria.per.ml. initial",
+                                        "ciliates.per.ml.initial", "flagellates.per.ml.initial", "rotifers.per.ml.initial",
+                                        "leafpack1.species1.mass.initial", "leafpack2.species1.mass.initial",
+                                        "leafpack1.species2.mass.initial", "leafpack2.species2.mass.initial",
+                                        "co2.final", "methane.final", "turbidity.final", "oxygen.percent.final",
+                                        "oxygen.conc.final", "ph.final", "chlorophyll.final", "bacteria.per.ml.final",
+                                        "ciliates.per.ml.final", "flagellates.per.ml.final", "rotifers.per.ml.final",
+                                        "leafpack1.species1.mass.final", "leafpack2.species1.mass.final",
+                                        "leafpack1.species2.mass.final", "leafpack2.species2.mass.final",
+                                        "water.volume.final", "fpom.final", "n15.bromeliad.final", "final.bromeliad.percentn",
+                                        "final.bromeliad.percentc"))
+  site_weather[[3]] <- site_weather[[3]][,okcols]
+
+
+  ## done
+  allsite <- rbind_all(site_weather)
+  return(allsite)
+}
+
