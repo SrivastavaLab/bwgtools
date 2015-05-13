5b38875070cdcbb953d60141124bcfeddfd504ef
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-05-13 11:18:52 -0700

    creates, exports, documents function for combining site.weather

--- R/combining.R
+++ R/combining.R
@@ -7,7 +7,7 @@ combine_site.info <- function() {
 
   site_info <- get_all_sites(sheetname = "site.info")
 
-  ### this could be a nice function
+  ### clean and message
   message("CLEANING: I'm taking only the first row of French Guiana. is that still necessary?")
   site_info[[4]] <- site_info[[4]][1, ] ## extra values from FG
   message("CLEANING: I'm taking only the first row of Columbia. is that still necessary?")
@@ -15,5 +15,21 @@ combine_site.info <- function() {
 
 
   allsite <- rbind_all(site_info)
+  return(allsite)
+}
+
+#' Obtain the site.weather data
+#'
+#' @return data.frame of all site.weather tabs
+#' @export
+combing_site.weather <- function(){
+  site_weather <- get_all_sites(sheetname = "site.weather")
+
+  ## clean and message
+  message("CLEANING: I'm dropping the 6th column from Argentina. is that still necessary?")
+  site_weather[[1]] <- site_weather[[1]][,-6]
+
+  ## done
+  allsite <- rbind_all(site_weather)
   return(allsite)
 }
