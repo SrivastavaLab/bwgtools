12f5c50f2094aff8bf9e8ac861f8121557086f3f
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-05-15 16:33:32 -0700

    move longwater to package

--- Rscripts/Nicos Working Script.R
+++ Rscripts/Nicos Working Script.R
@@ -27,15 +27,7 @@ str(mac)
 
 #function to turn the table into long format
 #MUST RUN
-longwater <- function(df) {
-  measures  <- df %>%
-    gather("data_name", "depth", starts_with("depth")) %>%
-    separate(data_name, into = c("depth_word", "leaf", "first_or_second","first")) %>%
-    select(-depth_word, -first) %>%
-    filter(!is.na(depth))
 
-  return(measures)
-}
 
 #USE NEXT FOR MACAE DATA ONLY!!!
 #add the day of the experiment plus the long format to the data
