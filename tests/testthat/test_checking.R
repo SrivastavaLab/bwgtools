b21a8d023705878092ae6bb27b75a0fb8298c6ce
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-05-22 09:56:28 -0700

    suppress messages

--- tests/testthat/test_checking.R
+++ tests/testthat/test_checking.R
@@ -4,7 +4,7 @@ context("checking data")
 
 test_that("data is checked correctly",{
 
-  testdat <- read_sheet(file = data, "leaf.waterdepths", ondisk = TRUE)
+  testdat <- suppressMessages(read_sheet(file = data, "leaf.waterdepths", ondisk = TRUE))
 
   names(testdat)[2] <- "foo"
   expect_warning(check_names(testdat), "trt.name is misnamed")
