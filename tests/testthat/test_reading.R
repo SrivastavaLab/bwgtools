b931a0e29409ddaeb022d21b2a7ac6abe035cd6a
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-05-22 09:39:06 -0700

    rearrange tests

--- tests/testthat/test_reading.R
+++ tests/testthat/test_reading.R
@@ -4,17 +4,18 @@ context("reading data")
 test_that("data is read correctly", {
   data <- system.file("extdata","Drought_data_PuertoRico.xlsx",
                       package = "bwgtools")
+  ## no sheet name
+  expect_error(read_sheet(file = data, ondisk = TRUE),
+               "c'mon give me a sheet name")
+  ## existing sheet name, ondisk file
   expect_message(read_sheet(file = data, "leaf.waterdepths",
                             ondisk = TRUE),
                  "you downloaded that file already! reading from disk")
-  expect_error(read_sheet(file = data, ondisk = TRUE),
-               "c'mon give me a sheet name")
-  testdat <- read_sheet(file = data, "leaf.waterdepths", ondisk = TRUE)
 
-
+  ## return value: a tbl_df
   stereotype <- dplyr::data_frame(a=1)
+
+  testdat <- read_sheet(file = data, "leaf.waterdepths", ondisk = TRUE)
   expect_equal(class(testdat), class(stereotype))
 
-  names(testdat)[2] <- "foo"
-  expect_warning(check_names(testdat), "trt.name is misnamed")
 })
