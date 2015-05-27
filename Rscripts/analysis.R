cb08b17cd02bdc4a2624afcc3e94a756716d4581
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-05-27 11:38:01 -0700

    :cold_sweat: corrected error caused by overwriting depth in summarise. vital to #16

--- Rscripts/analysis.R
+++ Rscripts/analysis.R
@@ -150,8 +150,8 @@ group_or_summarize <- function(data, aggregate_leaves = FALSE){
   if (aggregate_leaves) {
     data %>%
       group_by(site, watered_first, trt.name, site_brom.id, date) %>%
-      summarise(depth = mean(depth, na.rm = TRUE),
-                ndepth = sum(!is.na(depth)))
+      summarise(ndepth = sum(!is.na(depth)),
+                depth = mean(depth, na.rm = TRUE))
   } else {
     data %>%
       group_by(site, watered_first, trt.name, leaf)
@@ -161,13 +161,26 @@ group_or_summarize <- function(data, aggregate_leaves = FALSE){
 test_gr <- data_frame(site = c("macae", "macae", "macae", "macae", "macae"),
                       trt.name = c("mu3k0.5", "mu0.1k2",
                                    "mu1.5k0.5", "mu0.8k0.5", "mu0.1k2"),
-                      bromeliad.id = c("B5", "B29", "B34", "B8", "B29"),
-                      date = structure(c(1365206400, 1367452800, 1364688000, 1368230400,1368403200), class = c("POSIXct", "POSIXt"), tzone = "UTC"),
+                      site_brom.id = c("B5", "B29", "B34", "B8", "B29"),
+                      date = structure(c(1365206400, 1367452800, 1364688000, 1368230400,1367452800), class = c("POSIXct", "POSIXt"), tzone = "UTC"),
                       leaf = c("leafb", "centre",
                                "leafa", "leafb", "leafb"),
                       watered_first = c("yes", "yes", "yes", "yes", "yes"),
                       depth = c(74.8, 37, 56, 46.6, 10))
 
+
+
+test_gr <- data_frame(
+  main_grp = c("B5", "B29", "B34", "B8", "B29"),
+  sub_grp = c("leafb", "centre",
+           "leafa", "leafb", "leafb"),
+  depth = c(74.8, 37, 56, 46.6, 10))
+
+test_gr %>%
+  group_by(main_grp) %>%
+  summarise(ndepth = sum(!is.na(depth)),
+            depth = mean(depth, na.rm = TRUE),
+            n.depth = n())
 
 group_or_summarize(test_gr, TRUE)
 
@@ -177,6 +190,15 @@ group_or_summarize(test_gr, TRUE)
 
 group_or_summarize(test_gr, FALSE)
 
+sum(!is.na(1:2))
+
+
+data_frame(x = c("a", "a"),
+           z = c("b", "b"),
+           y = 1:2) %>%
+  group_by(x) %>%
+  summarise(ny = sum(!is.na(y)),
+            n.y = n())
 
 
 ## does grouped filter work the way i expect?
