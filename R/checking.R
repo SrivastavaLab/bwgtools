f82a6531c864b5157397fa97aa1e16b60c017fa2
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-06-22 16:11:33 -0700

    :fire: delete check_names

--- R/checking.R
+++ R/checking.R
@@ -1,25 +1,4 @@
-#' Checks names of a dataset
-#'
-#' could possibly be adapted for all datasets
-#'
-#' @param dataset the dataset in question (as a data.frame)
-#' @param column_names the correct names of that spreadsheet
-#' @export
-check_names <- function(dataset, column_names = c("site", "trt.name", "bromeliad.id", "date",
-                                                  "depth.centre.measure.first",
-                                                  "depth.leafa.measure.first",
-                                                  "depth.leafb.measure.first",
-                                                  "depth.centre.water.first",
-                                                  "depth.leafa.water.first",
-                                                  "depth.leafb.water.first")){
-  checks <- vapply(column_names, assertthat::has_name, TRUE, x = dataset)
-  problem <- column_names[!checks]
-  #browser()
-  if(!all(checks)) {
-    msg <- sprintf("%s is misnamed \n", problem)
-    warning(msg)
-  }
-}
+
 
 
 #' Fill in argentina data
@@ -76,7 +55,7 @@ names_all_same <- function(datalist){
 }
 
 
-#' Are the columns site an bromeliad.id found in this data.frame?
+#' Are the columns site and bromeliad.id found in this data.frame?
 #'
 #' @param df data frame to check for
 #'
