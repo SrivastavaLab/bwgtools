0731b94d51a81f9979c7f5d7a366ff35d198ada7
DEZERALDOlivier <olivier.dezerald@gmail.com>
2015-06-25 14:28:56 -0300

    attempt to create a documentation for the bwgtools package as a whole

---
+++ R/package-bwgtools.R
@@ -0,0 +1,22 @@
+#' bwgtools: A package for the bromeliad working group: the rainfall experiment
+#'
+#' The bwgtools package provides four categories of important functions:
+#' Accessing data, Combining data, Calulating metrics, and Analyze data.
+#' 
+#' @aliases bwg
+#' 
+#' @section Accessing data:
+#' \code{\link{offline}}
+#' 
+#' @section Combining data:
+#' \code{\link{combine_tab}}
+#' 
+#' @section Calculating metrics:
+#' \code{\link{hydro_variable}}
+#' 
+#' @section Analyzing data:
+#'  
+#' @docType package
+#' @name bwgtools
+NULL
+#> NULL
