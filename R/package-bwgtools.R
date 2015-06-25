a28b4765d4a3d11f1cfe364c4807a193ef13a9dc
DEZERALDOlivier <olivier.dezerald@gmail.com>
2015-06-25 16:29:43 -0300

    added subsections

--- R/package-bwgtools.R
+++ R/package-bwgtools.R
@@ -1,18 +1,33 @@
 #' bwgtools: A package for the bromeliad working group: the rainfall experiment
 #'
 #' The bwgtools package provides four categories of important functions:
-#' Accessing data, Combining data, Calulating metrics, and Analyze data.
+#' Accessing data, Combining data, Calulating metrics, and Analyzing data.
 #' 
-#' @aliases bwgtools
+#' 
+#' @seealso \url{https://github.com/SrivastavaLab/bwgtools/blob/master/README.Rmd}
 #' 
 #' @section Accessing data:
 #' \code{\link{offline}}
 #' 
 #' @section Combining data:
-#' \code{\link{combine_tab}}
+#' \itemize{
+#' \item \code{\link{combine_tab}}
+#' \item \code{\link{read_site_sheet}}
+#' }
 #' 
 #' @section Calculating metrics:
-#' \code{\link{hydro_variables}}
+#' \itemize{Hydrological metrics
+#' \item \code{\link{hydro_variables}}
+#' \item \code{\link{water_summary_calc}}
+#' }
+#' \itemize{Rainfall metrics
+#' }
+#' \itemize{Functional groups and trophic levels
+#' \item \code{\link{sum_func_groups}}
+#' }
+#' \itemize{Decomposition
+#' \item \code{\link{get_decomp}}
+#' }
 #' 
 #' @section Analyzing data:
 #' \code{\link{make_rlq}}
