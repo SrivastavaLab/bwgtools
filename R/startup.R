2e6a655813d0abcd21e5b2de0d234ba23fe6f68e
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-09-04 16:50:58 -0700

    modify the startup message to include version.

--- R/startup.R
+++ R/startup.R
@@ -1,5 +1,9 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to the bwg R package! in order to obtain data from the BWG dropbox folder, you need to authorize R to access your dropbox. run the following commands:\n  library(rdrop2)\n  drop_auth(cache = FALSE)\nThen enter your username and password. This should only need to be done when downloading the data.")
+  startblurb <- "Welcome to the bwg R package! in order to obtain data from the BWG dropbox folder, you need to authorize R to access your dropbox. run the following commands:\n  library(rdrop2)\n  drop_auth(cache = FALSE)\nThen enter your username and password. This should only need to be done when downloading the data.\n\nThis is bwgtools "
+  packageStartupMessage(startblurb,
+                        utils::packageDescription("bwgtools",
+                                                  field = "Version"),
+                        appendLF = TRUE)
 }
 
 # .onLoad <- function(libname, pkgname){
