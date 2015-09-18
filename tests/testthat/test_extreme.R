bce8631970889c229cc2f9d544a0b4a0152a0560
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-09-18 02:10:20 -0300

    tests for overflow and extremity

--- tests/testthat/test_extreme.R
+++ tests/testthat/test_extreme.R
@@ -33,3 +33,66 @@ test_that("extreme events are correctly ided",{
                          "overflow", "overflow", "overflow", "overflow"))
   
 })
+
+test_that("overflow is correct", {
+  testvec <- c(0, 50, NA, 100, 50)
+  expect_equal(bwgtools:::overflow(testvec), 0.25)
+  
+  
+  testvec2 <- c(100, NA,NA,NA)
+  expect_equal(bwgtools:::overflow(testvec2), 1)
+  
+  testvec3 <- c(0, 0, 0)
+  expect_equal(bwgtools:::overflow(testvec3), 0)
+  
+  testvec4 <- c(10, 10, 10)
+  expect_equal(bwgtools:::overflow(testvec4), 0)
+})
+
+
+test_that("oriedout is correct", {
+  testvec <- c(0, 50, NA, 100, 50)
+  expect_equal(bwgtools:::driedout(testvec), 0.25)
+  
+  
+  testvec2 <- c(100, NA,NA,NA)
+  expect_equal(bwgtools:::driedout(testvec2), 0)
+  
+  testvec3 <- c(0, 0, 0)
+  expect_equal(bwgtools:::driedout(testvec3), 1)
+  
+  testvec4 <- c(10, 10, 10)
+  expect_equal(bwgtools:::driedout(testvec4), 1)
+})
+
+test_that("extremity is correct", {
+  
+  testvec <- c(0, 50, NA, 100, 50)
+  expect_equal(bwgtools:::extremity(testvec), data_frame(event = c("driedout", "overflow"),
+                                                         prior = c(5L, 2L)))
+  
+  testvec2 <- c(100, NA,NA,NA)
+  expect_equal(bwgtools:::extremity(testvec2), data_frame(event = c("overflow"),
+                                                         prior = c(4L)))
+  
+  testvec3 <- c(0, 0, 0)
+  expect_equal(suppressWarnings(bwgtools:::extremity(testvec3)),
+               data_frame(event = c("driedout", "driedout", "driedout"),
+                          prior = c(3L, 2L, 1L)))
+  
+  expect_warning(bwgtools:::extremity(testvec3),
+               "boundaries are 0, 5, -10, 0. These are not increasing!
+                    Probably this leaf was too dry.
+                    Answer is drought, forever")
+  
+  testvec4 <- c(10, 10, 10)
+  expect_equal(suppressWarnings(bwgtools:::extremity(testvec4)),
+               data_frame(event = c("driedout", "driedout", "driedout"),
+                          prior = c(3L, 2L, 1L)))
+  
+  expect_warning(bwgtools:::extremity(testvec4),
+                 "boundaries are 0, 5, -10, 0. These are not increasing!
+                    Probably this leaf was too dry.
+                    Answer is drought, forever")
+})
+
